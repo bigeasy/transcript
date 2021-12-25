4b2df8feb6932a5978852de8fb52744d5643d562
Alan Gutierrez <alan@prettyrobots.com>
2021-12-24 23:03:55 -0600

    Add a dependency.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,20 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Person {
+    name: String,
+    age: usize,
+    verified: bool,
+}
+
 fn main() {
-    println!("hello, world");
+    let json = r#"
+        {
+            "name": "George",
+            "age": 27,
+            "verified": false
+        }
+    "#;
+    let person: Person = serde_json::from_str(json).unwrap();
+    println!("hello, {}", person.name);
 }
